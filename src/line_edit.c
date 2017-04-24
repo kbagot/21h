ad17223b91e52c00c3c9588ee727217e2ac687df
kbagot <kbagot@student.42.fr>
2017-04-24 21:17:21 +0200

    feature/copypaste-norme/move_by_word

--- src/line_edit.c
+++ src/line_edit.c
@@ -6,7 +6,7 @@
 /*   By: kbagot <kbagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/15 16:14:06 by kbagot            #+#    #+#             */
-/*   Updated: 2017/04/21 20:52:31 by kbagot           ###   ########.fr       */
+/*   Updated: 2017/04/24 21:14:12 by kbagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,11 @@ int	print(int c)
 
 static char	*delete_one(char *stin, t_data *data)
 {
-	data->cursor--;
-	if (data->cursor == 0)
-		stin = ft_strdup(&stin[1]);
-	else if (data->cursor == (int)ft_strlen(stin) - 1)
-		stin[data->cursor] = '\0';
+	if (data->cursor== (int)ft_strlen(stin))
+		stin[data->cursor - 1] = '\0';
 	else
-		stin = ft_strjoin(ft_strsub(stin, 0, data->cursor), &stin[data->cursor + 1]); //stin leaks
-//	printf("[%d] | [%s]", data->cursor, stin);
+		stin = ft_strjoin(ft_strsub(stin, 0, data->cursor - 1), &stin[data->cursor]); //stin leaks
+	//	printf("[%d] | [%s]", data->cursor, stin);
 	return (stin);
 }
 
@@ -35,7 +32,7 @@ static void	add_history(char *cmd, t_data *data)
 {
 	if (cmd && data->hist == NULL)
 	{
-//		ft_printf("salut1\n");
+		//		ft_printf("salut1\n");
 		data->hist = ft_memalloc(sizeof(t_hist));
 		data->hist->elem = ft_strdup(cmd);
 		data->hist->next = NULL;
@@ -43,7 +40,7 @@ static void	add_history(char *cmd, t_data *data)
 	}
 	else if (cmd && data->hist)
 	{
-	//	ft_printf("salut2\n");
+		//	ft_printf("salut2\n");
 		while (data->hist->next)
 			data->hist = data->hist->next;
 		data->hist->next = ft_memalloc(sizeof(t_hist));
@@ -52,6 +49,48 @@ static void	add_history(char *cmd, t_data *data)
 		data->hist->next->next = NULL;
 		data->hist = data->hist->next;
 	}
+}
+
+static void	word_left(t_data *data, char *stin)
+{
+	if ((data->cursor > 0 && ft_isspace(stin[data->cursor - 1]) &&
+				stin[data->cursor] > 32) || !stin[data->cursor])
+	{
+		tputs(tgetstr("le", NULL), 1, print);
+		data->cursor--;
+	}
+	while (stin[data->cursor] && data->cursor > 0)
+	{
+		if (ft_isspace(stin[data->cursor - 1]) && stin[data->cursor] > 32)
+			break;
+		tputs(tgetstr("le", NULL), 1, print);
+		data->cursor--;
+	}
+}
+
+static void	word_right(t_data *data, char *stin)
+{
+	if ((data->cursor == 0 || ft_isspace(stin[data->cursor - 1])) &&
+			stin[data->cursor] > 32)
+	{
+		tputs(tgetstr("nd", NULL), 1, print);
+		data->cursor++;
+	}
+	while (stin[data->cursor] && data->cursor > 0)
+	{
+		if (ft_isspace(stin[data->cursor - 1]) && stin[data->cursor] > 32)
+			break;
+		tputs(tgetstr("nd", NULL), 1, print);
+		data->cursor++;
+	}
+}
+
+static void	move_by_word(t_data *data, char *stin, char *buff)
+{
+	if (buff[0] == 1 && stin && data->cursor > 0)
+		word_left(data, stin);
+	else if (buff[0] == 4 && stin)//alt +right  ctrl + D
+		word_right(data, stin);
 }
 
 char		*line_edit(t_data *data)
@@ -66,90 +105,77 @@ char		*line_edit(t_data *data)
 	data->cursor = 0;
 	while (1)  // need \n
 	{
-		ft_bzero(buff, 4);
-		read(0, buff, 3);
-//	ft_printf("%d-%d-%d\n", buff[0], buff[1], buff[2]);
+		ft_bzero(buff, 6);
+		read(0, buff, 5);
+	//	ft_printf("lol : %d-%d-%d-%d-%d\n", buff[0], buff[1], buff[2], buff[3], buff[4]);
 		if (buff[0] == 27 && buff[1] == 91)//arrow
 			arrow_key(data, &stin, buff);
-		else if (buff[0] == 1)//alt +left  WORD MOVE ctrl + A
-		{
-			if (stin)
-			{
-				if (stin[data->cursor] == 9 || stin[data->cursor] == 32)
-				{
-					while (data->cursor > 0 && (stin[data->cursor] == 9 || stin[data->cursor] == 32))
-					{
-						tputs(tgetstr("le", NULL), 1, print);
-						data->cursor--;
-					}
-				}
-				else if (stin[data->cursor] != 9 || stin[data->cursor] != 32)
-				{
-					while (data->cursor > 0 && (stin[data->cursor] != 9 && stin[data->cursor] != 32))
-					{
-						tputs(tgetstr("le", NULL), 1, print);
-						data->cursor--;
-					}
-				}
-			}
-		}
-		else if (buff[0] == 4)//alt +right  ctrl + D
-		{
-			if (stin)
-			{
-				if (stin[data->cursor] == 9 || stin[data->cursor] == 32)
-				{
-					while (data->cursor < (int)ft_strlen(stin) && (stin[data->cursor] == 9 || stin[data->cursor] == 32))
-					{
-						tputs(tgetstr("nd", NULL), 1, print);
-						data->cursor++;
-					}
-				}
-				else if (stin[data->cursor] != 9 || stin[data->cursor] != 32)
-				{
-					while (data->cursor < (int)ft_strlen(stin) && (stin[data->cursor] != 9 && stin[data->cursor] != 32))
-					{
-						tputs(tgetstr("nd", NULL), 1, print);
-						data->cursor++;
-					}
-				}
-			}
-		}
-//		else if (buff[0] == 3)//ctrl + d   now ctrl+ C
-//		{
-//			init_term(RESTORE);
-//			tputs(tgetstr("ei", NULL), 1, print);
-//			exit(0);
-//		}
+		else if (buff[0] == 1 || buff[0] == 4)//alt +left  WORD MOVE ctrl + A
+			move_by_word(data, stin, buff);
+		//		else if (buff[0] == 3)//ctrl + d   now ctrl+ C
+		//		{
+		//			init_term(RESTORE);
+		//			tputs(tgetstr("ei", NULL), 1, print);
+		//			exit(0);
+		//		}
 		//ENTER CLIP MODE
-	/*	else if ()//copy mode //cut mode
+		else if (buff[0] == 9 && buff[1] == 0)//copy mode //cut mode  CTRL + I
 		{
 			int ce;
-			
-			data->cursor = ce;
-			read(0, buff, 3);
-			//fonction de mouvement
-			if (copy)
+
+			tputs(tgetstr("so", NULL), 1, print);
+			ce = data->cursor;
+			while (42)
 			{
-				if (ce >= data->cursor)
-					data->clipboard = ft_strsub(stin, data->cursor, ce);
-				else if (ce < data->cursor)
-					data->clipboard = ft_strsub(stin, ce, data->cursor);
-			}
-			else if (cut)
-			{
-				if (ce >= data->cursor)
+				ft_bzero(buff, 6);
+				read(0, buff, 5);
+				if (buff[0] == 27 && buff[1] == 91)//arrow
 				{
-					data->clipboard = ft_strsub(stin, data->cursor, ce);
-					stin = ft_strjoin(ft_strsub(stin, 0, data->cursor), &stin[ce]); //stin leaks
+					if (data->cursor >= 0 && data->cursor < (int)ft_strlen(stin))
+					{
+						tputs(tgetstr("ei", NULL), 1, print);
+						ft_putchar(stin[data->cursor]);
+						tputs(tgetstr("le", NULL), 1, print);
+						tputs(tgetstr("im", NULL), 1, print);
+					}
+					arrow_key(data, &stin, buff);
 				}
-				else if (ce < data->cursor)
+				else if (buff[0] == 1 || buff[0] == 4)//alt +left  WORD MOVE ctrl + A
+					move_by_word(data, stin, buff);
+				else if (buff[0] == 21 && buff[1] == 0)// copy  |   CTRL + U 
 				{
-					data->clipboard = ft_strsub(stin, ce, data->cursor);
-					stin = ft_strjoin(ft_strsub(stin, 0, ce), &stin[data->cursor + 1]); //stin leaks
+					if (ce >= data->cursor)
+						data->clipboard = ft_strsub(stin, data->cursor, ce);
+					else if (ce < data->cursor)
+						data->clipboard = ft_strsub(stin, ce, data->cursor);
+					break;
 				}
+				else if (buff[0] == 11 && buff[1] == 0)// cut       |      CTRL + K
+				{
+					if (ce >= data->cursor)
+					{
+						data->clipboard = ft_strsub(stin, data->cursor, ce);
+						stin = ft_strjoin(ft_strsub(stin, 0, data->cursor), &stin[ce]); //stin leaks
+					}
+					else if (ce < data->cursor)
+					{
+						data->clipboard = ft_strsub(stin, ce, data->cursor);
+						stin = ft_strjoin(ft_strsub(stin, 0, ce), &stin[data->cursor + 1]); //stin leaks
+					}
+					break;
+				}
+				else if (buff[0] == 27 && buff[1] == 0)//echap
+					break;
 			}
-		}*/
+			tputs(tgetstr("se", NULL), 1, print);
+		}
+		else if (buff[0] == 16 && buff[1] == 0)// PASTE   CTRL+P
+		{
+			ft_printf("%s", data->clipboard);
+			stin = ft_strjoin(ft_strjoin(ft_strsub(stin, 0, data->cursor - 1), data->clipboard),
+					&stin[data->cursor]); //stin leaks
+			data->cursor += ft_strlen(data->clipboard);
+		}
 		else if (buff[0] == 12)
 		{
 			tputs(tgetstr("cl", NULL), 1, print);
@@ -167,7 +193,7 @@ char		*line_edit(t_data *data)
 		{		//	ft_printf("%c", buff[0]);
 			if (data->cursor > 0)
 			{
-		// 		printf("salut\n");
+				// 		printf("salut\n");
 				stin = delete_one(stin, data);
 				tputs(tgetstr("le", NULL), 1, print);
 				tputs(tgetstr("dc", NULL), 1, print);
@@ -176,7 +202,7 @@ char		*line_edit(t_data *data)
 		}
 		else if (ft_isprint(buff[0]) && buff[1] == '\0')//maybe allow only printable
 		{
-		//	printf("l\n");
+			//	printf("l\n");
 			if (stin == NULL)
 				stin = ft_strdup(buff);
 			else if (data->cursor == (int)ft_strlen(stin)) // maybe -1
@@ -188,6 +214,7 @@ char		*line_edit(t_data *data)
 			ft_putchar(buff[0]);
 			data->cursor++;
 		}
+		//	printf("%s\n", stin);
 	}
 	ft_strdel(&buff);
 	tputs(tgetstr("ei", NULL), 1, print);
