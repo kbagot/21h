0409d7f31e324c3de4edf6aa0927cccdacf96f42
Kevin BAGOT <kbagot@e2r10p20.42.fr>
2017-09-06 19:52:48 +0200

    norme

--- src/exec_utility.c
+++ src/exec_utility.c
@@ -6,7 +6,7 @@
 /*   By: kbagot <kbagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 20:58:40 by kbagot            #+#    #+#             */
-/*   Updated: 2017/08/15 19:32:03 by kbagot           ###   ########.fr       */
+/*   Updated: 2017/09/06 14:40:10 by kbagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,8 @@ static void		find_pipe(char **cstin, int *j, int *k)
 	i = 0;
 	while (cstin[i] && ft_strcmp(cstin[i], "|") != 0)
 	{
-		if (!ft_strchr("\"\'", cstin[i][0] && (ft_strchr(cstin[i], '>') || ft_strchr(cstin[i], '<'))))
+		if (!ft_strchr("\"\'", cstin[i][0] && (ft_strchr(cstin[i], '>') ||
+						ft_strchr(cstin[i], '<'))))
 			*k += 1;
 		else
 			*j += 1;
@@ -142,13 +143,14 @@ static void		separate_line(char **cstin, int *i, t_line *line)
 {
 	int j;
 	int k;
+	char *c;
 
 	k = 0;
 	j = 0;
 	while (cstin[*i] && (ft_strcmp(cstin[*i], "|") != 0))
 	{//add redirect cmd
-		char *c;
-		if (!ft_strchr("\'\"", cstin[*i][0]) && ((c = ft_strchr(cstin[*i], '>')) || (c = ft_strchr(cstin[*i], '<'))))
+		if (!ft_strchr("\'\"", cstin[*i][0]) &&
+				((c = ft_strchr(cstin[*i], '>')) || (c = ft_strchr(cstin[*i], '<'))))
 		{
 			stock_redir(&cstin[*i], line, &j, &k);
 			*i += 1;
@@ -299,25 +301,24 @@ static int	dup_output(char **cmd, t_data *d, int i)
 	int n;
 	int w;
 
-	d->out = 1;
 	//printf("[%s][%s][%s] %d \n", cmd[0], cmd[1], cmd[2], i);
-	if (i != 1)
+	if (i != STDOUT_FILENO)
 		n = ft_atoi(cmd[0]);
 	else
-		n = 1;
+		n = STDOUT_FILENO;
 	if (!ft_strcmp(cmd[i], "-"))
 		close (n);
 	if (ft_isnbr(cmd[i]))
 	{
 		int t;
 		t = -1;
-	//ft_printf("%d\n", d->out);
+		//ft_printf("%d\n", d->out);
 		w = ft_atoi(cmd[i]);
-	//		printf("%d \n", d->out);
+		//		printf("%d \n", d->out);
 		if ((w >= 10 && w <= 12) || (t = dup(w)) == -1)
 		{
-	//		printf("%d \n", t);
-	//		printf("%d \n", w);
+			//		printf("%d \n", t);
+			//		printf("%d \n", w);
 			ft_putstr_fd("21sh: ", 2);
 			ft_putnbr_fd(w, 2);
 			ft_putstr_fd(": Bad file descriptor\n", 2);
@@ -325,28 +326,48 @@ static int	dup_output(char **cmd, t_data *d, int i)
 		}
 		if (t != -1)
 		{
-	//		printf("%ddelt \n", t);
+			//		printf("%ddelt \n", t);
 			close (t);
 		}
-			dup2(w, n);
+		dup2(w, n);
 	}
 	return (0);
 }
 
 static int	dup_input(char **cmd, t_data *d, int i)
 { //mirror of upper fct 
-	int up;
+	int n;
+	int w;
 
-	if (i != 1)
-		up = ft_atoi(cmd[0]);
+	//printf("[%s][%s][%s] %d \n", cmd[0], cmd[1], cmd[2], i);
+	if (i != STDIN_FILENO)
+		n = ft_atoi(cmd[0]);
 	else
-		up = 0;
+		n = STDIN_FILENO;
 	if (!ft_strcmp(cmd[i], "-"))
-		close (up);
-	else if (ft_isnbr(cmd[i]))
+		close (n);
+	if (ft_isnbr(cmd[i]))
 	{
-		d->out = ft_atoi(cmd[i]);
-		d->out = dup2(d->out, up);
+		int t;
+		t = -1;
+		//ft_printf("%d\n", d->out);
+		w = ft_atoi(cmd[i]);
+		//		printf("%d \n", d->out);
+		if ((w >= 10 && w <= 12) || (t = dup(w)) == -1)
+		{
+			//		printf("%d \n", t);
+			//		printf("%d \n", w);
+			ft_putstr_fd("21sh: ", 2);
+			ft_putnbr_fd(w, 2);
+			ft_putstr_fd(": Bad file descriptor\n", 2);
+			return (1);
+		}
+		if (t != -1)
+		{
+			//		printf("%ddelt \n", t);
+			close (t);
+		}
+		dup2(w, n);
 	}
 	return (0);
 }
@@ -374,10 +395,10 @@ static int exec_redir(char **rdr, t_data *d)
 			append_redir_output(cmd, d, i);
 		else if (i >= 0 && !ft_strcmp(cmd[i - 1], ">&"))
 		{if (dup_output(cmd, d, i))
-				return (1);}
+			return (1);}
 		else if (i >= 0 && !ft_strcmp(cmd[i - 1], "<&"))
 		{if (dup_input(cmd, d, i))
-				return (1);}
+			return (1);}
 		i = 0;
 		j++;
 	}
@@ -415,42 +436,32 @@ static void reset_fd(t_data *data)
 	close(data->stdout_cpy);
 	dup2(data->stderr_cpy, 2);
 	close(data->stderr_cpy);
-//	ft_printf("%d %d %d \n", data->in , data->out, data->err); Should keep it ?
-//	if (data->in != 0)
-//		close (data->in);
-//	if (data->out != 1)
-//		close (data->out);
-//	if (data->err != 2)
-//		close (data->err);
+	//	ft_printf("%d %d %d \n", data->in , data->out, data->err); Should keep it ?
+	//	if (data->in != 0)
+	//		close (data->in);
+	//	if (data->out != 1)
+	//		close (data->out);
+	//	if (data->err != 2)
+	//		close (data->err);
 }
 
-void		parse_entry(t_env **s_env, char **cstin, char *stin, t_data *data)
+static void	save_fd(t_data *data)
 {
-	t_env	*tmp_env;
-	t_line	*line;
-
-
-
-
 	data->stdin_cpy = 10;
 	data->stdout_cpy = 11;
 	data->stderr_cpy = 12;
-	dup2(0, 10);
-	dup2(1, 11);
-	dup2(2, 12);
-	data->in = 0;
-	data->out = 1;
-	data->err = 2;
-	line = split_pipe(cstin);
-	line = fork_pipes(line, data);
-	cstin = line->proc;
-	tmp_env = NULL;
+	dup2(STDIN_FILENO, 10);
+	dup2(STDOUT_FILENO, 11);
+	dup2(STDERR_FILENO, 12);
+	data->in = STDIN_FILENO;
+	data->out = STDOUT_FILENO;
+	data->err = STDERR_FILENO;
+}
 
-
-
-
-
-if ((data->rvalue = exec_redir(line->redirect, data)) || (data->rvalue = builtin(cstin, s_env, stin, data)))
+static int enter_builtin(t_data *data, t_env **s_env, char *stin, t_line *line)
+{
+	if ((data->rvalue = exec_redir(line->redirect, data)) ||
+			(data->rvalue = builtin(line->proc, s_env, stin, data)))
 	{
 		if (line->next)
 		{
@@ -459,27 +470,39 @@ if ((data->rvalue = exec_redir(line->redirect, data)) || (data->rvalue = builtin
 		else
 		{
 			reset_fd(data);
-			return ;
+			return (1);
 		}
 	}
+	return (0);
+}
 
+static void	enter_utility(t_data *data, t_env **s_env, t_line *line)
+{
+	t_env	*tmp_env;
 
-
-
-
-	tmp_env = master_env(*s_env, cstin, tmp_env);
-	if ((cstin = utility(cstin, *s_env)))
-		exec_utility(list_to_tab(tmp_env), cstin, data);
+	tmp_env = NULL;
+	tmp_env = master_env(*s_env, line->proc, tmp_env);
+	if ((line->proc = utility(line->proc, *s_env)))
+		exec_utility(list_to_tab(tmp_env), line->proc, data);
 	if (line->next)
 		_exit(data->rvalue);
 	destroy_env(&tmp_env);
 	ft_tabdel(&line->proc);
 	ft_tabdel(&line->redirect);
 	free(line);
+}
 
+void		parse_entry(t_env **s_env, char **cstin, char *stin, t_data *data)
+{
+	t_line	*line;
 
-
-
+	save_fd(data);
+	line = split_pipe(cstin);
+	line = fork_pipes(line, data);
+	if (!enter_builtin(data, s_env, stin, line))
+		enter_utility(data, s_env, line);
+	else
+		return ;
 	int status;
 	int pid;
 	while ((pid = wait(&status)) > 0)
--- src/splitforquote.c
+++ src/splitforquote.c
@@ -6,7 +6,7 @@
 /*   By: kbagot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/19 14:22:38 by kbagot            #+#    #+#             */
-/*   Updated: 2017/06/07 13:55:49 by kbagot           ###   ########.fr       */
+/*   Updated: 2017/09/06 19:51:40 by kbagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,31 @@ static int		f_l(char const *str, int i, char *c)
 		quote = str[i];
 		i++;
 		while (str[i] && str[i] != quote)
-		{	i++;
-		letter++;}
+		{	
+			i++;
+			letter++;
+		}
 		i++;
 	}
 	else
 		while (str[i] && (ft_strchr(c, str[i]) == NULL))
-		{i++;letter++;
+		{
+			i++;
+			letter++;
 			if (str[i] == '\'' || str[i] == '\"')
-				break ;}
+				break ;
+		}
 	return (letter);
+}
+
+static void		f_mnorm(int *quote, int *i, int *mot, char const *str)
+{
+	*quote = str[*i];
+	*i += 1;
+	while (str[*i] && str[*i] != *quote)
+		*i += 1;
+	*i += 1;
+	*mot += 1;
 }
 
 static int		f_m(char const *str, char *c)
@@ -46,33 +61,46 @@ static int		f_m(char const *str, char *c)
 	i = 0;
 	while (str[i])
 	{
-	//	printf("%c   %d\n", str[i], i);
 		while (str[i] && ft_strchr(c, str[i]))
 			i++;
 		quote = 0;
 		if (str[i] && (str[i] == '\"' || str[i] == '\''))
-		{
-		//printf("**%s   \n", &str[i]);
-			quote = str[i];
-			i++;
-			while (str[i] && str[i] != quote)
-				i++;
-			i++;
-		mot++;
-		}
+			f_mnorm(&quote, &i, &mot, str);
 		else if (str[i])
 		{
-	//	printf("$$%s   \n", &str[i]);
 			while (str[i] && (ft_strchr(c, str[i]) == NULL))
-			{	i++;
-			if (str[i] == '\'' || str[i] == '\"')
-				break ;}
-		mot++;}
+			{	
+				i++;
+				if (str[i] == '\'' || str[i] == '\"')
+					break ;
+			}
+			mot++;
+		}
 	}
-//	if (ft_strchr(c, str[ft_strlen(str)]))
-//		mot++;
-//		printf("WTF[%d]\n", mot);
 	return (mot);
+}
+
+static int  stock_q(int *i, int *j, char **split, char const *s)
+{
+	int		quote;
+	int		k;
+
+	k = 0;
+	quote = 0;
+	quote = s[*i];
+	if (s[*i] == '\"' || s[*i] == '\'')
+	{
+		split[*j][k++] = s[*i];
+		*i += 1;
+		while (s[*i] && s[*i] != quote)
+		{
+			split[*j][k++] = s[*i];
+			*i += 1;
+		}
+		*i += 1;
+		return (k);
+	}
+	return (0);
 }
 
 char			**splitforquote(char const *s, char *c)
@@ -81,52 +109,35 @@ char			**splitforquote(char const *s, char *c)
 	int		j;
 	int		k;
 	char	**split;
-	int		quote;
 
 	i = 0;
 	j = 0;
-	//	printf("s\n");
 	if (s == NULL || (split = malloc(sizeof(char*) * (f_m(s, c) + 1))) == NULL)
 		return (NULL);
-	//	printf("s1\n");
 	while (s[i])
 	{
 		k = 0;
 		while (s[i] && ft_strchr(c, s[i]))
 			i++;
-	//	printf("s2\n");
 		if (s[i])
 		{
 			if ((split[j] = malloc(sizeof(char) * (f_l(s, i, c) + 1))) == NULL)
 				return (NULL);
-	//	printf("s3\n");
-			quote = 0;
-			if (s[i] == '\"' || s[i] == '\'')
-			{
-				quote = s[i];
-		//printf("s4\n");
-				split[j][k++] = s[i++];
-				while (s[i] && s[i] != quote)
-					split[j][k++] = s[i++];
-				i++;
-		//printf("s5\n");
-			}
-			else
+			if (!(k = stock_q(&i, &j, split, s)))
 				while (s[i] && ft_strchr(c, s[i]) == NULL)
-				{if (s[i] == '\'' || s[i] == '\"')
-				break ;
-				split[j][k++] = s[i++];}
-	//	printf("s6\n");
+				{
+					if (s[i] == '\'' || s[i] == '\"')
+						break ;
+					split[j][k++] = s[i++];
+				}
 			split[j++][k++] = '\0';
-			//printf("%s\n", split[j - 1]);
 		}
-	//	printf("s7\n");
 	}
 	split[j] = NULL;
-return (split);
+	return (split);
 }
 /*
-int main(int ac, char **av)
-{
-	char **lol;if (ac == 2){	lol = strquotesplit(av[1], ";");if(lol)while(*lol){printf("[%s]\n", *lol);lol++;}}return (0);
-}*/
+   int main(int ac, char **av)
+   {
+   char **lol;if (ac == 2){	lol = strquotesplit(av[1], ";");if(lol)while(*lol){printf("[%s]\n", *lol);lol++;}}return (0);
+   }*/
